a65ad464f0827d108a14793a78b6510e27b729d8
kleber099 <kleber099@gmail.com>
2019-11-16 09:03:28 -0300

    Adicionando validaçao do token jwt nas rotas que necessitam estarem autenticadas

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import sessionController from './app/controllers/SessionController';
 import studentController from './app/controllers/StudentController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', sessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/students', studentController.store);
 routes.put('/students/:id', studentController.update);
